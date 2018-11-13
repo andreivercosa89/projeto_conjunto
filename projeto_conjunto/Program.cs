a43be7a0d40b601088698c108ee9105f3bd7cad7
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-13 19:45:02 -0400

    projeto conjunto

---
+++ projeto_conjunto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace projeto_conjunto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+
+            HashSet<string> lista = new HashSet<string>();
+
+
+            do
+            {
+                Console.Write("Informe o nome a ser add: ");
+                lista.Add(Console.ReadLine().Trim().ToUpper());
+
+                Console.Write("Digite sair ou der enter para continuar : ");
+                texto = Console.ReadLine();
+
+
+
+            } while (texto != "sair");
+
+            
+
+            foreach (var l in lista) { Console.Write(" " + l); }
+        }
+    }
+}
